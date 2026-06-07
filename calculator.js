e0e862ae844271e8aa297c1a4f18b3ac3f57761d
NicolasMatheisen <nicolasmatheisen@icloud.com>
2026-06-07 12:14:54 +0200

    Eventt Handler bauen

--- calculator.js
+++ calculator.js
@@ -1,1 +1,22 @@
 const display = document.getElementById('display');
+const operator = '';
+const firstOperand = null;
+const secondOperand = null;
+
+document.querySelectorAll('.button').forEach(
+    button => {
+        button.addEventListener(
+            'click', () => {
+                const action = button.dataset.action;
+                const number = button.dataset.number;
+
+                if (number) {
+                    handleNumberInput(number);
+                }
+                else (action) {
+                    handleActionInput(action);
+                }
+            }
+        );
+    }
+);
